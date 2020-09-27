8f67b2c654055fc4321cf27235fe9851661cdf87
galib130 <galibshams16@gmail.com>
2020-09-27 16:52:08 +0600

    search and drawer

--- app/src/main/java/com/example/drawing/MainActivity.kt
+++ app/src/main/java/com/example/drawing/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.drawing
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.view.*
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toolbar
+import android.widget.*
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -16,6 +17,11 @@ import com.OrdersFragment
 import com.ProfileFragment
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import  androidx.appcompat.widget.SearchView
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import java.util.ArrayList
+
 
 class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
 
@@ -23,6 +29,11 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
     lateinit var cartFragment: CartFragment
     lateinit var profileFragment: ProfileFragment
     lateinit var ordersFragment: OrdersFragment
+
+    val list: MutableList<String> = ArrayList()
+    val list2= arrayOf<String>("C++","Java","Coa","Google","Facebook","Kotlin","Javascript")
+
+   var result=""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,7 +60,19 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
             .replace(R.id.fragment_container,homeFragment)
             .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
             .commit()
+        list.addAll(list2)
+        var adapter=ArrayAdapter(this,android.R.layout.simple_list_item_1,list)
+        autocomplete.threshold=1
+        autocomplete.setAdapter(adapter)
+
+        autocomplete.setOnItemClickListener { adapterView, view, i, l ->
+             result=autocomplete.text.toString()
+            val toast=Toast.makeText(applicationContext,"You've selected "+result,Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
     }
+
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
@@ -60,6 +83,7 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
                     .replace(R.id.fragment_container,homeFragment)
                     .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                     .commit()
+
             }
             R.id.nav_cart ->{
                 cartFragment= CartFragment()
@@ -68,6 +92,7 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
                     .replace(R.id.fragment_container,cartFragment)
                     .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                     .commit()
+
             }
             R.id.nav_profile ->{
                 profileFragment= ProfileFragment()
@@ -76,6 +101,7 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
                     .replace(R.id.fragment_container,profileFragment)
                     .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                     .commit()
+
             }
             R.id.nav_orders ->{
                 ordersFragment= OrdersFragment()
@@ -100,5 +126,47 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
             super.onBackPressed()
 
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search,menu)
+        val manager= getSystemService(Context.SEARCH_SERVICE)as SearchManager
+        val searchItem= menu?.findItem(R.id.action_search)
+        val searchView =searchItem?.actionView as SearchView
+
+
+            searchView.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    searchView.clearFocus()
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    if(newText!!.isNotEmpty())
+                    {
+                       list.clear()
+                        val search=newText.toLowerCase()
+                        list2.forEach {
+                                if(it.toLowerCase().contains(search))
+                                {
+                                     list.add(it)
+
+                                }
+                        }
+                    }
+                    else
+                    {
+
+
+                    }
+                    return true
+                }
+
+            })
+
+
+
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
